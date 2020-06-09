fbdb9e7cb8bf392c50622d2a319c802310096f42
oriel <orielmalihi1@gmail.com>
2020-06-10 00:04:45 +0300

    filterfalse is working

--- Demo.cpp
+++ Demo.cpp
@@ -42,14 +42,14 @@ int main(int argc, const char * argv[]) {
         cout << i << " ";  // Hello HelloBye HelloByeAdam
     cout << endl << endl;
 
-    // cout << endl << "accumulate of range with binary operator: " << endl;
-	// for (int i: accumulate(range(5,9), [](int x, int y){return x*y;}))
-	// 	cout << i << " ";      // 5 30 210 1680
+    cout << endl << "accumulate of range with binary operator: " << endl;
+	for (int i: accumulate(range(5,9), [](int x, int y){return x*y;}))
+		cout << i << " ";      // 5 30 210 1680
 
-    // cout << "####  Filter False:  ####";
-    // cout << endl << "Filter out all numbers less than 3 in vector{1,2,3,4}: " << endl;
-    // for (auto i: filterfalse(lessThan3{}, vecInt) )
-    //     cout << i << " ";   // 3 4
+    cout << "\n####  Filter False:  ####";
+    cout << endl << "Filter out all numbers less than 3 in vector{1,2,3,4}: " << endl;
+    for (auto i: filterfalse(lessThan3{}, vecInt) )
+        cout << i << " ";   // 3 4
     // cout << endl << "Filter out all even numbers in range(5,9): " << endl;
     // for (auto i: filterfalse([](int i){return i%2==0;}, range(5,9)) )
     //     cout << i << " ";   // 5 7
